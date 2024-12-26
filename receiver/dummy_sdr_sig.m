668c6dab9111c6e82b05b3c9f29f4eade8d286c1
handu789 <3038119671@qq.com>
2024-12-26 20:53:53 +0800

    Delete receiver directory

--- receiver/dummy_sdr_sig.m
+++
@@ -1,7 +0,0 @@
-function sig_channel = dummy_sdr_sig(message_transmit, snr, phy)
-    
-    symbols = phy.encode(message_transmit);
-    sig = phy.modulate(symbols);
-    sig_channel = awgn([zeros(100000,1);sig;zeros(100000,1);sig;zeros(100000,1)], snr-10*log10(phy.fs/phy.bw));
-
-end
